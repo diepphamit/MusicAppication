7981ff541799d21e081b44387e60af626e83d5d9
ha16t2 <ntha5531@gmail.com>
2020-08-04 17:26:39 +0700

    commit tam

--- Frontend/src/app/user/sign-in/sign-in.component.ts
+++ Frontend/src/app/user/sign-in/sign-in.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +10,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent implements OnInit {
-
-  constructor() { }
+  loginForm: FormGroup;
+  submitted = false;
+  notification;
+  constructor(private formBuilder: FormBuilder, private route: Router, private userService: UserService) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(9)]],
+
+    }, {
+
+    });
+
+
+  }
+
+
+  get f() {
+    return this.loginForm.controls;
+  }
+  onSubmitLogin() {
+    this.submitted = true;
+    if (this.loginForm.invalid) {
+      return;
+    }
+
   }
 
 }
+
+
